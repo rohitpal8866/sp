f42ed8cc61c829f1624d4d649512b3dcbad837b4
Rohit Pal <rohitpal8866317504@gmail.com>
2025-01-29 11:47:02 +0530

    Add Favicon

--- resources/views/admin/include/layout.blade.php
+++ resources/views/admin/include/layout.blade.php
@@ -15,7 +15,7 @@
     <link rel="stylesheet" href="{{ asset('assets/vendors/perfect-scrollbar/perfect-scrollbar.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/vendors/bootstrap-icons/bootstrap-icons.css')}} ">
     <link rel="stylesheet" href="{{ asset('assets/css/app.css') }}">
-    <link rel="shortcut icon" href="{{ asset('favicon.ico')}}" type="image/x-icon">
+    <link rel="shortcut icon" href="{{ asset('favicon.png')}}" type="image/x-icon">
     <link rel="stylesheet" href="{{ asset('assets/vendors/sweetalert2/sweetalert2.min.css')}}">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fancyapps/ui@5.0/dist/fancybox/fancybox.css" />
     @stack('styles')
